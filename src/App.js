ced72526bd8a92d2ab421edb6166ac612a97ebbb
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-06 09:21:40 +0300

    Labrit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,84 @@
+import "./App.css"
 import Diena from "./Diena";
 
 
 
-function App() {
-  const pirmdienasStundas = [
+// function App() {
+//   const pirmdienasStundas = [
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :(",
+//     "Pupsiks :)",
+//     "Pupsiks :)"
+//   ]
+//   const otrdienasStundas = [
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :("
+//   ]
+//   const tresdienasStundas = [
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :(",
+//     "Pupsiks :)",
+//     "Pupsiks :)"
+//   ]
+//   const ceturtdienasStundas = [
+//     "Pupsiks :)",
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :("
+//   ]
+//   const piektdienasStundas = [
+//     "Nav Pupsiks :(",
+//     "Nav Pupsiks :("
+//   ]
+
+const visuDienuStunduSaraksts = [
+  {
+    diena: "Pirmdiena",
+    stundas: [
     "Nav Pupsiks :(",
     "Nav Pupsiks :(",
-    "Pupsiks",
-    "Pupsiks"
-  ]
-  const otrdienasStundas = [
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :("
-  ]
-  const tresdienasStundas = [
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :(",
-    "Pupsiks",
-    "Pupsiks"
-  ]
-  const ceturtdienasStundas = [
-    "Pupsiks",
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :("
-  ]
-  const piektdienasStundas = [
-    "Nav Pupsiks :(",
-    "Nav Pupsiks :("
-  ]
+    "Pupsiks :)",
+    "Pupsiks :)"
+    ]
+  },
+  {
+    diena: "Otrdiena",
+    stundas: [
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :("
+    ]
+  }
+  {
+    diena: "Tresdiena",
+    stundas: [
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :(",
+      "Pupsiks :)",
+      "Pupsiks :)"
+    ]
+  },
+  {
+    diena: "Ceturtdiena",
+    stundas: [
+      "Pupsiks :)",
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :("
+    ]
+  },
+  {
+    diena: "Piektdiena",
+    stundas: [
+      "Nav Pupsiks :(",
+      "Nav Pupsiks :("
+    ]
+  },
+]
+
   return (
     
     <>
